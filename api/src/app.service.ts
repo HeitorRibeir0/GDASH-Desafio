79b065a7302d5f305d1995b58069b30b5cd5c7d4
Heitor <heitorrmatos0159@gmail.com>
2025-12-03 20:21:57 -0300

    chore: create and add test endpoint to verify database connection

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Weather, WeatherDocument } from './schema/weather.schema';
 
 @Injectable()
 export class AppService {
-  constructor(private)
-  
+
+    constructor(
+    @InjectModel(Weather.name) private weatherModel: Model<WeatherDocument>
+  ) {}
+
+  async createTest() {
+    return this.weatherModel.create({
+      temperature_2m:25.5,
+      weather_code: 1,
+      relative_humidity_2m: 60
+    });
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
 }
